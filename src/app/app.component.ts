7f6875393644084455c21f4714d7e59f72904abe
Mohamed Hassan <mohamed>
2023-03-24 15:43:51 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { GoogleLoginProvider, SocialAuthService } from "@abacritt/angularx-social-login";
+import { FacebookLoginProvider } from "@abacritt/angularx-social-login";
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {Auth, FacebookAuthProvider, GoogleAuthProvider} from "firebase/auth" ;
+import {AngularFireAuth } from '@angular/fire/compat/auth' ;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-packages';
+  user: any;
+ 
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+    });
+  }
+  signInWithFB(): void {
+    this.authService.signIn(GoogleAuthProvider.PROVIDER_ID).then(res => {
+      console.log(res)
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AngularFireModule} from '@angular/fire/compat'
+import {AngularFireAuthModule} from '@angular/fire/compat/auth'
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,45 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBrKsjZv-j_EgWKUJE21fa1jThm25HQQr4",
+      authDomain: "sociallogin-1744d.firebaseapp.com",
+      projectId: "sociallogin-1744d",
+      storageBucket: "sociallogin-1744d.appspot.com",
+      messagingSenderId: "1048526602451",
+      appId: "1:1048526602451:web:ceb1294fcc53c74141d426",
+      measurementId: "G-JGRY2ZN5HQ"
+    }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '1048526602451-efdtkes83qbi4c0es9fa42gebeejc34j.apps.googleusercontent.com'
+              
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('5268710979893896')
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
